bd225907d3594deb9c26fff6e367eb1853fb4f23
RAFAEL BARROS <rafa.a.barros@hotmail.com>
2024-07-19 10:25:47 -0300

    Update FIIS.sql

--- FIIS.sql
+++ FIIS.sql
@@ -1,7 +1,15 @@
-CREATE TABLE ticker
-  (id INT NOT NULL AUTO_INCREMENT, acronym VARCHAR(6) NOT NULL, active BOOLEAN, PRIMARY KEY(id), UNIQUE(acronym));
+CREATE TABLE ticker(
+  id INT NOT NULL AUTO_INCREMENT,
+  acronym VARCHAR(6) NOT NULL,
+  active BOOLEAN,
+  detailFund_id INT NOT NULL,
+  classification_id INT NOT NULL,
+  administration_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  fee_id INT NOT NULL,
+  PRIMARY KEY(id),
+  Unique(acronym));
 
-  
 CREATE TABLE detailFund(
   id INT NOT NULL AUTO_INCREMENT,
   fundName VARCHAR(100),
@@ -9,29 +17,25 @@ CREATE TABLE detailFund(
   numQuotaHolders INT,
   ipoDate DATE,
   fundSite VARCHAR(100),
-  percentageCash DECIMAL,
+  percentageCash DECIMAL(100),
   typeManagement BOOLEAN,
   numQuotas INT,
   cnpj BIGINT,
   hasDeadline BOOLEAN,
   targetAudience_id INT NOT NULL,
   rating DECIMAL,
-  ticker_id INT NOT NULL,
-  PRIMARY KEY(id)
-);
+  PRIMARY KEY(id));
 
-ALTER TABLE detailFund
-  ADD CONSTRAINT ticker_detailFund
-    FOREIGN KEY (ticker_id) REFERENCES ticker (id);
+ALTER TABLE ticker
+  ADD CONSTRAINT detailFund_ticker
+    FOREIGN KEY (detailFund_id) REFERENCES detailFund (id);
 
 CREATE TABLE targetAudience
   (id INT NOT NULL AUTO_INCREMENT, nameAudience VARCHAR(50), PRIMARY KEY(id));
-
+  
 ALTER TABLE detailFund
   ADD CONSTRAINT targetAudience_detailFund
     FOREIGN KEY (targetAudience_id) REFERENCES targetAudience (id);
-
-
 
 CREATE TABLE historicalData(
   id INT NOT NULL AUTO_INCREMENT,
@@ -50,40 +54,23 @@ CREATE TABLE Dividend(
   id INT NOT NULL AUTO_INCREMENT,
   comDate DATE,
   exDate DATE,
-  value DECIMAL,
+  price DECIMAL,
   ticker_id INT NOT NULL,
   PRIMARY KEY(id)
 );
 
 ALTER TABLE Dividend
-  ADD CONSTRAINT ticker_Dividend FOREIGN KEY (ticker_id) REFERENCES ticker (id)
-  ;
+  ADD CONSTRAINT ticker_Dividend FOREIGN KEY (ticker_id) REFERENCES ticker (id);
 
 CREATE TABLE fee(
   id INT NOT NULL AUTO_INCREMENT,
   managementFee DECIMAL,
   AdmFee DECIMAL,
   performanceFee DECIMAL,
-  ticker_id INT NOT NULL,
-  PRIMARY KEY(id)
-);
+  PRIMARY KEY(id));
 
-ALTER TABLE fee
-  ADD CONSTRAINT ticker_fee FOREIGN KEY (ticker_id) REFERENCES ticker (id);
-
-CREATE TABLE administration(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100),
-  cnpj BIGINT,
-  phone INT,
-  email VARCHAR(100),
-  ticker_id INT NOT NULL,
-  PRIMARY KEY(id)
-);
-
-ALTER TABLE administration
-  ADD CONSTRAINT ticker_administration FOREIGN KEY (ticker_id) REFERENCES ticker (id)
-  ;
+ALTER TABLE ticker
+  ADD CONSTRAINT fee_ticker FOREIGN KEY (fee_id) REFERENCES fee (id);
 
 CREATE TABLE manager(
   id INT NOT NULL AUTO_INCREMENT,
@@ -91,20 +78,39 @@ CREATE TABLE manager(
   cnpj BIGINT,
   phone INT,
   email VARCHAR(100),
-  ticker_id INT NOT NULL,
-  PRIMARY KEY(id)
-);
+  PRIMARY KEY(id));
 
-ALTER TABLE manager
-  ADD CONSTRAINT ticker_manager FOREIGN KEY (ticker_id) REFERENCES ticker (id);
+ALTER TABLE ticker
+  ADD CONSTRAINT manager_ticker FOREIGN KEY (manager_id) REFERENCES manager (id);
 
-CREATE TABLE classification(
+CREATE TABLE administration(
   id INT NOT NULL AUTO_INCREMENT,
-  name INT,
-  ticker_id INT NOT NULL,
-  PRIMARY KEY(id)
-);
+  name VARCHAR(100),
+  cnpj BIGINT,
+  phone INT,
+  email VARCHAR(100),
+  PRIMARY KEY(id));
 
-ALTER TABLE classification
-  ADD CONSTRAINT ticker_classification
-    FOREIGN KEY (ticker_id) REFERENCES ticker (id);
+ALTER TABLE ticker
+  ADD CONSTRAINT administration_ticker
+    FOREIGN KEY (administration_id) REFERENCES administration (id);
+
+CREATE TABLE classification(id INT NOT NULL AUTO_INCREMENT, name INT, PRIMARY KEY(id));
+
+ALTER TABLE ticker
+  ADD CONSTRAINT classification_ticker
+    FOREIGN KEY (classification_id) REFERENCES classification (id);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
